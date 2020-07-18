4e7744f650a1d0a6dbca189faa2e426e9ae8692b
unknown <Salvador@LAPTOP-34JVTPVR.tagtechnologies.us>
2020-07-18 01:09:36 -0600

    Alertify

--- Spa/src/app/nav/nav.component.ts
+++ Spa/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() { }
@@ -18,9 +19,11 @@ export class NavComponent implements OnInit {
   // tslint:disable-next-line: typedef
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in!');
+      // console.log('Logged in!');
+      this.alertify.success('Welcome!');
     }, error => {
       // console.log('Failed to Login');
+      this.alertify.error(error);
       console.log(error);
     });
   }
@@ -35,5 +38,6 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     console.log('Good bye');
+    this.alertify.message('Good bye')
   }
 }
